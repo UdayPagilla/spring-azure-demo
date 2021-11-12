fb143f71944ac2a70fa87f4628307a5e3eaacfe7
UdayPagilla <pagillauday@gmail.com>
2021-11-12 09:39:53 +0530

    azure uday commit

---
+++ src/main/java/com/javafsd/azureuday/AzureUdayApplication.java
@@ -0,0 +1,20 @@
+package com.javafsd.azureuday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class AzureUdayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureUdayApplication.class, args);
+	}
+	@GetMapping("/welcome")
+	public String welcomeAzure() {
+		return "welcome to azure";
+	}
+
+}
---
+++ src/test/java/com/javafsd/azureuday/AzureUdayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javafsd.azureuday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureUdayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
